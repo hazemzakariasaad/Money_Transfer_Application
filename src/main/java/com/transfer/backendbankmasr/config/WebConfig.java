43264e143501044bcbee6ce1b2882c4cdce50766
KhaledBadr07 <khaledbadr0722@gmail.com>
2024-09-09 16:13:21 +0300

    create webconfig

---
+++ src/main/java/com/transfer/backendbankmasr/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.transfer.backendbankmasr.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // Allow CORS on all paths
+                .allowedOrigins("*")  // Allow specific origin(s) by adding hosts
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Allow specific HTTP methods
+                .allowedHeaders("*")  // Allow all headers
+                .allowCredentials(true);  // Allow credentials (like cookies)
+    }
+}
